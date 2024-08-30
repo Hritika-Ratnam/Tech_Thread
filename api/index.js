ba1c02e76b13ec5f10c92e7141649d7868bbde5e
Hritika-Ratnam <najukreso@gmail.com>
2024-08-30 19:03:36 +0530

    Add a middleware and a func to hadle errors.

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-//backend remeber to add .js
+//backend remember to add .js in last
 
 dotenv.config();
 
@@ -26,3 +26,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req , res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+  
+});
