1170a6eb8d3cf1d385f1a9e39cec32e5adff3621
Hritika-Ratnam <najukreso@gmail.com>
2024-09-10 11:54:02 +0530

    .

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,35 +14,29 @@ export default function Home() {
     };
     fetchPosts();
   }, []);
-
   return (
-    <div className='bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-100 min-h-screen'>
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-3xl font-bold lg:text-6xl text-teal-600 dark:text-teal-400'>
-          Welcome to My Blog
-        </h1>
-        <p className='text-gray-600 dark:text-gray-300 text-xs sm:text-sm'>
+    <div>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>
           Here you'll find a variety of articles and tutorials on topics such as
           web development, software engineering, and programming languages.
         </p>
         <Link
           to='/search'
-          className='text-xs sm:text-sm text-teal-500 dark:text-teal-300 font-bold hover:underline'
+          className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
         >
           View all posts
         </Link>
       </div>
-
-      <div className='p-6 bg-teal-100 dark:bg-slate-700'>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
         <CallToAction />
       </div>
 
-      <div className='max-w-6xl mx-auto p-6 flex flex-col gap-8 py-7'>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
         {posts && posts.length > 0 && (
           <div className='flex flex-col gap-6'>
-            <h2 className='text-2xl font-semibold text-center text-teal-700 dark:text-teal-300'>
-              Recent Posts
-            </h2>
+            <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
             <div className='flex flex-wrap gap-4'>
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
@@ -50,7 +44,7 @@ export default function Home() {
             </div>
             <Link
               to={'/search'}
-              className='text-lg text-teal-600 dark:text-teal-300 hover:underline text-center'
+              className='text-lg text-teal-500 hover:underline text-center'
             >
               View all posts
             </Link>
