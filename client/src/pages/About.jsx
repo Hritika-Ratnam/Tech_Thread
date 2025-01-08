0cd7b7b80906aada2974be09f5d7809704a4d841
hritka ratnam <najukreso@gmail.com>
2025-01-08 23:50:53 +0530

    text align center

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
             About Tech Thread
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
-            <p style="text-align: left;">
+            <p style={{textAlign: "left"}}>
             Welcome to Tech Thread! Created as a personal project by a passionate developer, 
             Tech Thread aims to share ideas, insights, and experiences in the world of technology.
              Whether it's web development, software engineering, or programming languages, 
@@ -16,7 +16,7 @@ export default function About() {
              tutorials, and tips to help you stay ahead in the tech world.
             </p>
 
-            <p style="text-align: left;">
+            <p style={{textAlign: "left"}}>
             Tech Thread isn't just about sharing knowledge; it's about building a community. 
             We encourage you to interact by leaving comments, liking posts, and engaging with other readers. 
             By learning together, we believe that we can all grow, improve, and keep evolving in this ever-changing field. 
